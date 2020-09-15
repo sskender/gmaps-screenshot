1737cd2ede2c670f7e85a0f49baed3f196c29e5d
sskender <sskender@users.noreply.github.com>
2020-09-15 13:25:13 +0200

    coordinates split to lat and lng arguments

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const captureWebsite = require('capture-website');
 
 const BASE_URL = 'https://google.com/maps/place';
 
-// load coordinates as 'lat+lng' parameter
-const coordinates = process.argv[2];
+// load coordinates as 'lat' and 'lng' parameters
+const lat = process.argv[2];
+const lng = process.argv[3];
+
+const coordinates = `${lat}+${lng}`;
 console.log(`Screenshot for coordinates ${coordinates}`);
 
 // craft url and execute
 const URL = `${BASE_URL}/${coordinates}`;
+
 (async () => {
     await captureWebsite.file(URL, `photos/img-${coordinates}.png`);
 })();
